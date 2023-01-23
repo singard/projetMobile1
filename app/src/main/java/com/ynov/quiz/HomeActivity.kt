175fc52e3a167b3e61e43050211140cb7bad3621
singard <mathieugonza3@gmail.com>
2023-01-23 18:49:04 +0100

    create home page

---
+++ app/src/main/java/com/ynov/quiz/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.ynov.quiz
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class HomeActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+    }
+}
