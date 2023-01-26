0a5c40840f1f514530798159e4ed5f22c6f2605f
singard <mathieugonza3@gmail.com>
2023-01-26 16:14:27 +0100

    changement du fond d'écran + image + changement de la page par défalt

--- app/src/main/java/com/ynov/quiz/HomeActivity.kt
+++ app/src/main/java/com/ynov/quiz/HomeActivity.kt
@@ -1,18 +1,24 @@
 package com.ynov.quiz
 
-import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 class HomeActivity : AppCompatActivity(){
 
+    private lateinit var homeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val homeButton: Button = findViewById(R.id.homeButton)
 
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
